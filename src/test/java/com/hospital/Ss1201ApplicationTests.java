7c942986196c1f3d66056d9cae44a7ca3f887e6c
Bui tuan Giang <gbuif12@gmail.com>
2026-05-11 15:41:36 +0700

    ss12_01

---
+++ src/test/java/com/hospital/Ss1201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss1201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
